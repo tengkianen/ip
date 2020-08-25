dfe3905cd801c32a4f48f182a4590ada7ee91164
tengkianen <tengkianen@gmail.com>
2020-08-25 10:56:47 +0800

    Implemented skeletal version of Duke that greets the user, echoes commands and exit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,11 +8,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String line;
+
         System.out.println("----------------------------------------------------------------------\n");
         System.out.println("Hello I'm Duke\n");
         System.out.println("What can I do for you?\n");
         System.out.println("----------------------------------------------------------------------\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("----------------------------------------------------------------------\n");
+
+        while(true) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+
+            if (line.equals("bye")) {
+                System.out.println("----------------------------------------------------------------------\n");
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("----------------------------------------------------------------------\n");
+                break;
+            } else {
+                System.out.println("----------------------------------------------------------------------\n");
+                System.out.println(line);
+                System.out.println("----------------------------------------------------------------------\n");
+            }
+        }
     }
+
 }
